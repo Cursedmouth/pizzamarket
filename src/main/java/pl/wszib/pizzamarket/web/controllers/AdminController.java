3296700ca26525d489baab63ee50915b931bb450
Cursedmouth <piotr.jarosz.87@gmail.com>
2022-06-27 22:07:10 +0200

    9_Stworzenie AdminController

---
+++ src/main/java/pl/wszib/pizzamarket/web/controllers/AdminController.java
@@ -0,0 +1,13 @@
+package pl.wszib.pizzamarket.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("admin/orders")
+    public String showAdminOrdersPage() {
+        return "adminOrdersPage";
+    }
+}
